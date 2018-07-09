ce0bd8ff5b5f1e508dc48a89655c1e138cb49f86
shifeng <shifeng199307@gmail.com>
2018-07-09 18:02:57 +0800

    sync

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 
-// 引入根类
-import './src/App.dart';
+// 引入路由
+import './routes/index.dart';
+
+// 引入store
+import './store/index.dart';
+import './store/states/AppState.dart';
+
+class App extends StatelessWidget {
+  final store = createStore();
+  App();
+
+  @override
+  Widget build(BuildContext context) {
+    return new StoreProvider<AppState>(
+      store: createStore(),
+      child: new Routes()
+    );
+  }
+}
 
 // 渲染
 void main() => runApp(new App());
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_demo/src/App.dart';
+import 'package:flutter_demo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
