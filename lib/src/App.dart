5cc7c66b96a0143f9b3c144b414cf069d90f6d95
shifeng <shifeng199307@gmail.com>
2018-07-09 17:34:40 +0800

    sync

--- lib/src/App.dart
+++ lib/src/App.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+
+// 引入store
+import './store/index.dart';
+import './store/states/AppState.dart';
+// 引入页面
 import './pages/index.dart';
 
-class App extends StatefulWidget {
-  @override
-  AppState createState() => new AppState();
-}
+class App extends StatelessWidget {
+  final store = createStore();
+  App();
 
-class AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      routes: <String, WidgetBuilder>{
-        '/': (BuildContext context) => new HomeScreen(),
-        '/My': (BuildContext context) => new MyScreen(),
-        '/Cart': (BuildContext context) => new CartScreen(),
-      },
+    return new StoreProvider<AppState>(
+      store: store,
+      child: new MaterialApp(
+        theme: new ThemeData.dark(),
+        routes: <String, WidgetBuilder>{
+          '/': (BuildContext context) => new HomeScreen(),
+          '/My': (BuildContext context) => new MyScreen(),
+          '/Cart': (BuildContext context) => new CartScreen(),
+        },
+      ),
     );
   }
 }
