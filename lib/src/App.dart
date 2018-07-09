1b07bfe7ba31c93161010a14c61282d6bb96b392
shifeng <shifeng199307@gmail.com>
2018-07-09 17:52:43 +0800

    sync

--- lib/src/App.dart
+++ lib/src/App.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 
+// 引入路由
+import './routes/index.dart';
+
 // 引入store
 import './store/index.dart';
 import './store/states/AppState.dart';
-// 引入页面
-import './pages/index.dart';
 
 class App extends StatelessWidget {
   final store = createStore();
@@ -14,15 +15,8 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new StoreProvider<AppState>(
-      store: store,
-      child: new MaterialApp(
-        theme: new ThemeData.dark(),
-        routes: <String, WidgetBuilder>{
-          '/': (BuildContext context) => new HomeScreen(),
-          '/My': (BuildContext context) => new MyScreen(),
-          '/Cart': (BuildContext context) => new CartScreen(),
-        },
-      ),
+      store: createStore(),
+      child: new Routes()
     );
   }
 }
